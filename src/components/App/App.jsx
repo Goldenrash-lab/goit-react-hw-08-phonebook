ed9f8c8f4aac7011015c7116d25591b2c84b2b8d
Yehor Hryhortsev <egor.goldenrash@gmail.com>
2023-12-27 19:04:43 +0200

    final

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import { Route, Routes, useNavigate } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import RegisterPage from 'pages/RegisterPage';
 import LoginPage from 'pages/LoginPage';
 import ContactsPage from 'pages/ContactsPage';
@@ -8,7 +8,6 @@ import PrivateRoute from 'guards/PrivateRoute';
 import PublicRoute from 'guards/PublicRoute';
 import { useDispatch } from 'react-redux';
 import { refreshThunk } from 'store/auth/thunks';
-import { toast } from 'react-toastify';
 
 export const App = () => {
   const dispatch = useDispatch();
